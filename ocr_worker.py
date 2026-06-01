1187edf77e474e01e427096afc4be098127f86ce
Pablo Roque <ingpabloroque@gmail.com>
2026-06-01 17:06:02 -0300

    fix: eliminar ruta tesseract Windows

--- ocr_worker.py
+++ ocr_worker.py
@@ -12,8 +12,6 @@ from PIL import Image, ImageOps
 import pytesseract
 import os
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
 app = Flask(__name__)
 CORS(app)
 
